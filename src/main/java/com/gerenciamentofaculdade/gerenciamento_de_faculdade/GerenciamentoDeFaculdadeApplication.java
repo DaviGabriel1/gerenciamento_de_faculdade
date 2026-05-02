7eb78ce26812cb3362b6df2bdb38a4dd49290fb8
DaviGabriel1 <davig41441503@gmail.com>
2026-05-01 21:24:13 -0300

    Initial commit

---
+++ src/main/java/com/gerenciamentofaculdade/gerenciamento_de_faculdade/GerenciamentoDeFaculdadeApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciamentofaculdade.gerenciamento_de_faculdade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeFaculdadeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciamentoDeFaculdadeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gerenciamentofaculdade/gerenciamento_de_faculdade/GerenciamentoDeFaculdadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciamentofaculdade.gerenciamento_de_faculdade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeFaculdadeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
